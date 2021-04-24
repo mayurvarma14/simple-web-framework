4d21d0c20f0e283e8b8b8b0e43a57fd37fb990af
Mayur Varma <mayurvarma14@gmail.com>
2021-04-24 20:01:36 +0530

    refactor to extract model class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import { User } from './models/User';
 
-const user = new User({ name: "John", age: 34 });
+// const user = new User({ name: "John", age: 34 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
-// user.save();
-user.events.on('change', () => { console.log('User was changed'); });
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+// // user.save();
+// user.events.on('change', () => { console.log('User was changed'); });
 
-user.set({ age: 22 });
-const user1 = new User({ id: 1 });
-user1.events.on('change', () => { console.log('User was changed', user1); });
-user1.fetch();
+// user.set({ age: 22 });
+// const user1 = new User({ id: 1 });
+// user1.events.on('change', () => { console.log('User was changed', user1); });
+// user1.fetch();
 
 
-const user2 = new User({ name: 'Leo', age: 46 });
-user2.on('save', () => {
-    console.log('User was saved', user2);
+// const user2 = new User({ name: 'Leo', age: 46 });
+// user2.on('save', () => {
+//     console.log('User was saved', user2);
+// });
+// // user2.save();
+
+const user3 = User.buildUser({ name: "Faye Champlin", age: 21 });
+
+user3.on('save', () => {
+    console.log('User was saved', user3);
 });
-// user2.save();
+
+// user3.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,8 @@
 import { AxiosResponse } from 'axios';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Models';
+import { ApiSync } from './ApiSync';
 interface UserProps {
     id?: number;
     name?: string;
@@ -9,50 +10,17 @@ interface UserProps {
 }
 
 const rootUrl = 'http://localhost:3000/users';
-export class User {
-
-    public events: Eventing = new Eventing();
-    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-    public attributes: Attributes<UserProps>;
-
-    constructor(attributes: UserProps) {
-        this.attributes = new Attributes<UserProps>(attributes);
+export class User extends Model<UserProps> {
+    static buildUser(attributes: UserProps): User {
+        return new User(
+            new Attributes<UserProps>(attributes),
+            new Eventing(),
+            new ApiSync<UserProps>(rootUrl)
+        );
     }
+    // constructor(attributes: UserProps) {
+    //     this.attributes = new Attributes<UserProps>(attributes);
+    // }
 
-    get on() {
-        return this.events.on;
-    }
 
-    get trigger() {
-        return this.events.trigger;
-    }
-
-    get get() {
-        return this.attributes.get;
-    }
-
-    set(update: UserProps) {
-        this.attributes.set(update);
-        this.events.trigger('change');
-    }
-
-    fetch() {
-        const id = this.get('id');
-        if (typeof id !== 'number') {
-            throw new Error('Cannot fetch without an id');
-        }
-        this.sync.fetch(id).then((response: AxiosResponse) => {
-            this.set(response.data);
-        });
-    }
-    save(): void {
-
-        this.sync.save(this.attributes.getAll())
-            .then((response: AxiosResponse) => {
-                this.trigger('save');
-            })
-            .catch(() => {
-                this.trigger('error');
-            });
-    }
 }
